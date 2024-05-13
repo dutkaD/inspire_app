2e1ed3eeddd50a85f7df0e23f73fa9011f84d37a
ddut <diana.dutka.partner@idv-ag.de>
2024-05-13 21:19:54 +0200

    add a stateless widget

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,31 @@
+import 'package:daily_quotes_app/service/inspirational_quote_service.dart';
+import 'package:daily_quotes_app/service/quote_card.dart';
+import 'package:flutter/material.dart';
+
+import 'model/inspirational_quote.dart';
 
 void main() {
-  // try out code here or run the tests
+  var service = InspirationService();
+  List<InspirationalQuote> quotes = service.getInspiringQuotes();
 
-  // FLUTTER! do not uncomment YET!
-  // runApp(
-  //     const MaterialApp(home: Text("Fanciest app of them all"),)
-  // );
+  runApp(MaterialApp(
+    home: Scaffold(
+        appBar: AppBar(
+          backgroundColor: Colors.blue,
+          centerTitle: true,
+          title: const Text("Inspiration App"),
+        ),
+        body: Center(
+          child: Column(
+            children: [
+              const Text("My favorite quotes"),
+              Column(
+                mainAxisAlignment: MainAxisAlignment.start,
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: quotes.map((e) => QuoteCard(title: e.text,)).toList(),
+              ),
+            ],
+          ),
+        )),
+  ));
 }
