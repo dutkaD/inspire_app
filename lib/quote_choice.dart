230f18d1129512d6ab4ef4ea5de5fef128607068
ddut <diana.dutka.partner@idv-ag.de>
2024-05-13 22:11:23 +0200

    add states, shuffle and switch between views

--- lib/quote_choice.dart
+++ lib/quote_choice.dart
@@ -1,4 +1,5 @@
 import 'package:daily_quotes_app/quote_list.dart';
+import 'package:daily_quotes_app/service/inspirational_quote_service.dart';
 import 'package:flutter/material.dart';
 import 'model/inspirational_quote.dart';
 
@@ -12,8 +13,88 @@ class QuoteChoice extends StatefulWidget {
 }
 
 class _QuoteChoiceState extends State<QuoteChoice> {
+  final InspirationService service = InspirationService();
+  bool _showAll = true;
+
+  // we promise that it won't be null
+  late InspirationalQuote quote;
+
+  @override
+  initState() {
+    super.initState();
+    updateRandomQuote();
+  }
+
+  void updateRandomQuote() {
+    setState(() {
+      quote = service.getRandomQuote(widget.quotes);
+    });
+  }
+
+  void toggleShowAll() {
+    setState(() {
+      _showAll = !_showAll;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
-    return AllQuotesList(quotes: widget.quotes);
+    return Column(
+      children: [
+        Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: ElevatedButton(
+              onPressed: () {
+                toggleShowAll();
+              },
+              child: Text(
+                _showAll ? "Random view" : "All quotes",
+                style: const TextStyle(fontSize: 20),
+              )),
+        ),
+        _showAll
+            ? AllQuotesList(quotes: widget.quotes)
+            : Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Center(
+                    child: Card(
+                      color: const Color.fromRGBO(239, 142, 252, 0.2),
+                      child: Padding(
+                        padding: const EdgeInsets.all(8.0),
+                        child: Column(
+                          children: [
+                            const Text("Random Quote"),
+                            Column(
+                              children: [
+                                IconButton(
+                                    onPressed: () {
+                                      updateRandomQuote();
+                                    },
+                                    icon: const Icon(Icons.rotate_right_sharp)),
+                                Column(
+                                  children: [
+                                    Text(
+                                      quote.text,
+                                      textAlign: TextAlign.center,
+                                      style: const TextStyle(fontSize: 35),
+                                    ),
+                                    Text(
+                                      quote.author,
+                                      style: const TextStyle(color: Colors.white, fontSize: 25),
+                                    )
+                                  ],
+                                ),
+                              ],
+                            ),
+                          ],
+                        ),
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+      ],
+    );
   }
 }
